cffaae55b37d8ce657b9e7f97284e52218ad848d
mahfujulAppnap <58552730+mahfujulAppnap@users.noreply.github.com>
2024-02-27 20:52:42 +0600

    Initial Commit

---
+++ Inspections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inspections
+//
+//  Created by Appnap Mahfuj on 27/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
